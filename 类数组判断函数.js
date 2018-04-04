81e3a5c426d035e45fc61042e6306f2ad22dccb1
TorryXC <taowei.wang@me.com>
2018-04-04 23:49:50 +0800

    Update 类数组判断函数.js

--- 类数组判断函数.js
+++ 类数组判断函数.js
@@ -1,7 +1,7 @@
 function isArrayLikeObject(alo) {
   if (alo &&                          //不是null undefined 空字符串等
     typeof alo === "object" &&        //是对象
-    ifFinite(alo.length) &&           //长度有限数值
+    isFinite(alo.length) &&           //长度有限数值
     alo.length >= 0 &&                //长度非负
     alo.length === Math.floor(alo.length) &&     //长度整数
     alo.length < 4294967296)     //长度小于2^32
